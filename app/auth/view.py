8462c8ece82ec0f60256b86354a096217ebb4a99
Dmitry E <erkov_dmitriy@mail.ru>
2022-10-24 04:18:54 +0300

    Changed custom login form

--- app/auth/view.py
+++ app/auth/view.py
@@ -1,19 +1,26 @@
 from app import *
-from flask import render_template
+from flask import render_template, request
 from .form import LoginForm
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', title="Главная")
+    return render_template('index.html', title="Main")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        print("OK")
-    return render_template('login.html', title="Авторизация с помощью Custom forms")
+    message = ''
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('psw')
+        # simple checking a sending of form
+        if email == 'e@mail.ru' and password == 'pass':
+            message = "Correct username and password"
+        else:
+            message = "Wrong username or password"
+        print("Custom Form - is ok")
+    return render_template('login.html', message=message, title="Using Custom html forms")
 
 
 @app.route("/login_quick_form", methods=["GET", "POST"])
@@ -22,7 +29,7 @@ def login_quick_form():
 
     if form.validate_on_submit():
         print("QuickForm - is ok")
-    return render_template('login_quick_form.html', form=form, title="Авторизация с помощью Quick_form()")
+    return render_template('login_quick_form.html', form=form, title="Using Quick_form()")
 
 
 @app.route('/login_custom_wtforms', methods=["POST", "GET"])
@@ -30,4 +37,4 @@ def login_custom_wtforms():
     form = LoginForm()
     if form.validate_on_submit():
         print("Custom WTForms - is ok")
-    return render_template('login_custom_wtforms.html', form=form, title="Авторизация с помощью Custom WTForms")
+    return render_template('login_custom_wtforms.html', form=form, title="Using WTForms with custom styles")
